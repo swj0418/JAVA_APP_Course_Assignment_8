58cffd53d23460d71402b523c467fecb22bd977c
swj0418 <swj0418@hotmail.com>
2017-10-02 23:01:46 +0900

    commit

---
+++ Assignment_8_Final/assignment_8/src/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class Calculator {
+	public Calculator(){
+	}
+	public static Double Addition(Double x, Double y)
+	{
+		return x + y;
+	}
+	public static Double Deduction(Double x, Double y)
+	{
+		return x - y;
+	}
+	public static Double Multiplication(Double x, Double y)
+	{
+		return x * y;
+	}
+	public static Double Division(Double x, Double y)
+	{
+		return x / y;
+	}
+}
---
+++ Assignment_8_Final/assignment_8/src/calculator/Converter.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Converter {
+	public Converter() {
+		
+	}
+	public static Double Convert(String s)
+	{
+		return Double.parseDouble(s);
+	}
+}
